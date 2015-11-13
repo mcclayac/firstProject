79efc8b48ce1672bb7ef753a9e3c196a9d172dac
Anthony McClay <anthony.mcclay@nielsen.com>
2015-11-13 11:15:04 -0500

    Initial commit

---
+++ src/com/mcclayac/Main.java
@@ -0,0 +1,10 @@
+package com.mcclayac;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("This is my first inteliJ application");
+        System.out.println("Tony McClay");
+
+    }
+}
